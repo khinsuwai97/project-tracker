2af8e0e988824fffd14c01569369ddfeceeb65c1
khinsuwai97 <khinsuwai009@gmail.com>
2025-10-13 21:51:14 +0700

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center text-xl mt-4">Project Tracker</h1>
+    </div>
+  );
+};
+
+export default App;
